f0149c361193d1f6c147f9752d8ae5268ff2cc6a
sofuetakuma <kawahagi0620@gmail.com>
2023-01-30 04:29:19 +0900

    3.6実装

--- src/repositories.rs
+++ src/repositories.rs
@@ -6,6 +6,7 @@ use std::{
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use validator::Validate;
 
 #[derive(Debug, Error)]
 enum RepositoryError {
@@ -42,13 +43,17 @@ impl Todo {
 
 // TodoRepositoryを利用するHttpリクエストメソッド用の構造体
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct CreateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     pub(crate) text: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct UpdateTodo {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    #[validate(length(max = 100, message = "Over text length"))]
     text: Option<String>,
     completed: Option<bool>,
 }
